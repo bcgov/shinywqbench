1e588362ffe4bef4aadfe88924fe1e92dc4d781f
Ayla Pearson <ayla@poissonconsulting.ca>
2023-03-02 17:14:38 -0800

    able to generate basic html report

--- R/mod-summary.R
+++ R/mod-summary.R
@@ -6,7 +6,11 @@ mod_summary_ui <- function(id, label = "summary") {
       h2("Summary"),
       br(),
       br(),
-      br()
+      br(),
+      downloadButton(
+        outputId = ns("report"),
+        label = "Generate report"
+      )
     )
   )
 }
@@ -21,9 +25,45 @@ mod_summary_server <- function(id, ext) {
       rv <- reactiveValues(
       )
       
+      
      
+      
+      output$report <- downloadHandler(
+        filename <-  "ecotox_report.html",
+        
+        content = function(file) {
+          tempReport <- file.path(tempdir(), "summary-report.Rmd")
+          print(getwd())
+          file.copy(
+            system.file("extdata/summary-report.Rmd", package = "shinywqbench"), 
+            tempReport, overwrite = TRUE
+          )
+          
+          params <- list(
+            input_a = 1901,
+            input_b = 2
+          )
+          
+          rmarkdown::render(
+            tempReport, 
+            output_file = file,
+            params = params,
+            envir = new.env(parent = globalenv())
+          )
+        }
+      )
+      
       
     }
   )
 }
 
+
+# output$dl_aggregated <- downloadHandler(
+#   filename = function() {
+#     paste0(input$file_raw, ".csv")
+#   },
+#   content = function(file) {
+#     readr::write_csv(rv$aggregated, file)
+#   }
+# )
