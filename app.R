2c4bf148a0bdabb0815cc64bbd335aaabd5573e9
Ayla Pearson <ayla@poissonconsulting.ca>
2023-03-30 15:36:52 -0700

    removing notes

--- app.R
+++ app.R
@@ -1,15 +1,2 @@
 pkgload::load_all(".")
 shinywqbench::run_wqbench_app()
-
-
-# Titanium oxide - has a bunch of points
-# Benzene - has bc wqg
-
-# (1,1-Dimethylethyl)ethenylbenzene
-
-#1000984359 VF
-
-## error with plot
-# 145307171
-
-#(1E)-N-[(6-Chloro-3-pyridinyl)methyl]-N'-cyano-N-methylethanimidamide
