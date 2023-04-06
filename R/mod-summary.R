b91b8bc56be1b23e20833dd39aa40460901fde5f
Ayla Pearson <ayla@poissonconsulting.ca>
2023-04-06 13:33:16 -0700

    fixing errors in file generation on tab 3 when no chemical is selected

--- R/mod-summary.R
+++ R/mod-summary.R
@@ -46,17 +46,35 @@ mod_summary_server <- function(id, ext1, ext2) {
             system.file("extdata/summary-report.Rmd", package = "shinywqbench"), 
             tempReport, overwrite = TRUE
           )
-          params <- list(
-            chem_name = stringr::str_squish(ext2$name),
-            cas_num = ext2$cas,
-            af_table = ext2$af_table,
-            af = ext2$af,
-            trophic_sp_table = ext2$trophic_sp_table,
-            trophic_grp_table = ext2$trophic_grp_table,
-            method = ext2$method,
-            benchmark = ext2$bench,
-            gp_result = ext2$gp_results
-          )
+          
+          if (!is.null(ext2$name)) {
+            params <- list(
+              chem_name = stringr::str_squish(ext2$name),
+              cas_num = ext2$cas,
+              af_table = ext2$af_table,
+              af = ext2$af,
+              trophic_sp_table = ext2$trophic_sp_table,
+              trophic_grp_table = ext2$trophic_grp_table,
+              method = ext2$method,
+              benchmark = ext2$bench,
+              gp_result = ext2$gp_results
+            )
+          } else {
+            # for when no benchmark has been calculated
+            params <- list(
+              chem_name = "",
+              cas_num = "",
+              af_table = NULL,
+              af = NA_real_,
+              trophic_sp_table = NULL,
+              trophic_grp_table = NULL,
+              method = "",
+              benchmark = data.frame(ctv_est_mg.L = NA_real_),
+              gp_result = NULL
+            )
+          }
+          
+    
           rmarkdown::render(
             tempReport, 
             output_file = file,
@@ -72,16 +90,17 @@ mod_summary_server <- function(id, ext1, ext2) {
           file_name_dl("data-summary", ext2$cas, "xlsx")
         },
         content = function(file) {
-          sheets <- list(
-            raw = filter_data_raw_dl(ext2$raw),
-            selected = filter_data_raw_dl(ext2$selected),
-            aggregate_data = filter_data_agg_dl(ext2$agg),
-            assessment_factor = ext2$af_table,
-            ctv = ext2$bench
-          )
-          if (is.null(ext1$data)) {
+          if (is.null(ext2$raw)) {
             sheets <- list(
-              note = data.frame(x = "no chemical selected")
+              note = data.frame(x = "no benchmark generated")
+            )
+          } else {
+            sheets <- list(
+              raw = filter_data_raw_dl(ext2$raw),
+              selected = filter_data_raw_dl(ext2$selected),
+              aggregate_data = filter_data_agg_dl(ext2$agg),
+              assessment_factor = ext2$af_table,
+              ctv = ext2$bench
             )
           }
           writexl::write_xlsx(sheets, file)
