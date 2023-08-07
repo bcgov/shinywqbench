a19b477262ab71f37a22d4bb51e67ba551325364
Ayla Pearson <ayla@poissonconsulting.ca>
2023-08-07 13:28:01 -0700

    correct character issue

--- R/app-ui.R
+++ R/app-ui.R
@@ -20,7 +20,7 @@ app_ui <- function() {
     shinyjs::useShinyjs(),
     waiter::useWaiter(),
     navbarPage(
-      title = "Emergent Contaminant Aquatic Life Benchmarks – DRAFT",
+      title = "Emergent Contaminant Aquatic Life Benchmarks - DRAFT",
       selected = "tab1",
       id = "navbarID",
       tabPanel(
